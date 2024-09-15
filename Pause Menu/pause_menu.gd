adc68edaa1fc8774c3905047b7c1365a0d89df92
Daniel Jagga <79388224+danieljagga@users.noreply.github.com>
2024-09-15 05:24:46 -0400

    added pause function

--- Pause Menu/pause_menu.gd
+++ Pause Menu/pause_menu.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-
-
-
+func _on_change_world_pressed() -> void:
+	get_tree().change_scene_to_file("res://world_selection.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
