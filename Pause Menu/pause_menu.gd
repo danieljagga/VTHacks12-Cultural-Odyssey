fc5cc389da41f26e496a243da880c3091697e57f
rayhu2003 <raymondhu2006@gmail.com>
2024-09-14 21:01:10 -0400

    init
    Check notes in paus menu when finalizing, need to add background, and swap scene to world select.

--- Pause Menu/pause_menu.gd
+++ Pause Menu/pause_menu.gd
@@ -6,3 +6,8 @@ extends Node2D
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _input(event: InputEvent) -> void:
+	if(event.is_action_pressed("ui_cancel")):
+		get_tree().change_scene_to_file("res://Pause Menu/pause_menu.tscn")
+		#put these three lines on all of the worlds 
